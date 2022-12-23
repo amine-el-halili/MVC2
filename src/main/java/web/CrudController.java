c337dfc4662728ee09a98ab071e193c967f5cafa
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-12-23 17:33:28 +0100

    ...

--- src/main/java/web/CrudController.java
+++ src/main/java/web/CrudController.java
@@ -8,17 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.SQLException;
+
 
 @WebServlet("/hi")
 public class CrudController extends HttpServlet {
-    ProductDaoImpl productDao;
-    String path;
-    public CrudController() throws SQLException, ClassNotFoundException {
-        productDao = new ProductDaoImpl();
-    }
 
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.getWriter().println(req.getPathInfo());
+        res.getWriter().println(req.getRequestURI().split("/")[2]);
     }
 }
